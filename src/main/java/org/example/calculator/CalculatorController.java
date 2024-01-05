a6a429ad7dbc12c74bfbd9680c5be00634632a11
Evelyn Cassinotte <e.cass03@gmail.com>
2024-01-05 19:58:06 -0300

    first commit

---
+++ src/main/java/org/example/calculator/CalculatorController.java
@@ -0,0 +1,84 @@
+package org.example.calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CalculatorController {
+    @FXML
+    private Label lblScreenEquation;
+    @FXML
+    private Label lblScreenCalculation;
+    @FXML
+    protected void onClickBtnZero(){
+
+    }
+    @FXML
+    protected void onClickBtn01(){
+
+    }
+    @FXML
+    protected void onClickBtn02(){
+
+    }
+    @FXML
+    protected void onClickBtn03(){
+
+    }
+    @FXML
+    protected void onClickBtn04(){
+
+    }
+    @FXML
+    protected void onClickBtn05(){
+
+    }
+    @FXML
+    protected void onClickBtn06(){
+
+    }
+    @FXML
+    protected void onClickBtn07(){
+
+    }
+    @FXML
+    protected void onClickBtn08(){
+
+    }
+    @FXML
+    protected void onClickBtn09(){
+
+    }
+    @FXML
+    protected void onClickBtnAC(){
+
+    }
+    @FXML
+    protected void onClickBtnEqual(){
+
+    }
+    @FXML
+    protected void onClickBtnMinus(){
+
+    }
+    @FXML
+    protected void onClickBtnPlus(){
+
+    }
+    @FXML
+    protected void onClickBtnMultiplication(){
+
+    }
+    @FXML
+    protected void onClickBtnDivision(){
+
+    }
+    @FXML
+    protected void onClickBtnPercentage(){
+
+    }
+    @FXML
+    protected void onClickBtnDot(){
+
+    }
+}
